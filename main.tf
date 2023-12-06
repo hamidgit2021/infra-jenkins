13c755982387e06f992755b00d590f531b96f6cd
hamidgit2021 <79046793+hamidgit2021@users.noreply.github.com>
2023-12-06 11:18:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -175,7 +175,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
 # Create an EKS cluster
 resource "aws_eks_cluster" "hamid_cluster" {   # previously was karo_cluster
   name     = "hamid_cluster"
-  role_arn = aws_iam_role.hamid_cluster_role.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
   version  = "1.26"
 
   vpc_config {
